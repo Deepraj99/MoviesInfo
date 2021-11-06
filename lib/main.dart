9c765876af36205baf8ea402dfad7b7a5db4e277
Deepak Prajapati <dp0774138@gmail.com>
2021-11-06 14:59:42 +0530

    forth commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,15 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:moviesinfo/data/core/api_client.dart';
-import 'package:moviesinfo/data/data_sources/movie_remote_data_source.dart';
-import 'package:moviesinfo/data/repositories/movie_repository_imple.dart';
 import 'package:moviesinfo/domain/entities/app_error.dart';
 import 'package:moviesinfo/domain/entities/movie_entity.dart';
 import 'package:moviesinfo/domain/entities/no_params.dart';
-import 'package:moviesinfo/domain/repositories/movie_repositories.dart';
 import 'package:moviesinfo/domain/usecases/get_trending.dart';
+import 'package:pedantic/pedantic.dart';
+import 'di/get_it.dart' as getIt;
 
 Future<void> main() async {
-  ApiClient apiClient = ApiClient(Client());
-  MovieRemoteDataSource dataSource = MovieRemoteDataSourceImpl(apiClient);
-  MovieRepository movieRepository = MovieRepositoryImpl(dataSource);
-  // movieRepository.getTrending();
-  GetTrending getTrending = GetTrending(movieRepository);
+  unawaited(getIt.init());
+  GetTrending getTrending = getIt.getItInstance<GetTrending>();
   final Either<AppError, List<MovieEntity>> eitherResponse =
       await getTrending(NoParams());
   eitherResponse.fold((l) {
@@ -25,10 +19,6 @@ Future<void> main() async {
     print('list of movies');
     print(r);
   });
-  // dataSource.getTrending();
-  // dataSource.getPopular();
-  // dataSource.getPlayingNow();
-  // dataSource.getCommingSoon();
   runApp(MyApp());
 }
 
